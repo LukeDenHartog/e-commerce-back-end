e6534a09e771dbe246fd0042d77b5ae2a48ca90f
Luke <121516961+Afrozez@users.noreply.github.com>
2023-07-09 11:02:08 -0700

    tested and adjusted category/tag routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,7 +5,7 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const categories = await Category.findAll( {include: [{ model: Product}] } )
+    const categories = await Category.findAll( { include: [{ model: Product}] } )
     res.status(200).json(categories);
   }catch (err) {
     res.status(500).json({ message: 'Could not find all of the categories and/or its associated Products' });
@@ -21,45 +21,43 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: 'id not found' });
       return;
     }
-
     res.status(200).json(category);
   } catch (err) {
     res.status(500).json({ message: 'Internal server error!' });
   }
-  // find one category by its `id` value
-  // be sure to include its associated Products
 });
-
+/* For the post/put/delete routes the req.body should look similar the comment below.
+    {
+      "category_name" : "flip flops"
+    } 
+*/
 
 router.post('/', async (req, res) => {
   try {  // create a new category
    const newCat = await Category.create(req.body);
+
    res.status(200).json(newCat);
   } catch (error) {
     res.status(500).json({ message: 'Bad Request: Invalid parameters or request syntax.' });
   }
- 
 });
 
 router.put('/:id', async (req, res) => {
-  
   try { // this code snippet is using Sequelize to update a category from a database table based on the provided id in the request parameters.
-  const updatedCategory = await Category.update(req.body, {where: {id: req.params.id } })
+  const updatedCategory = await Category.update(req.body, { where: {id: req.params.id } })
   res.status(200).json(updatedCategory);
-} catch {
-  res.status(500).json({ message: 'Bad Request: Invalid parameters or request syntax.' });
-}
- 
+  } catch {
+    res.status(500).json({ message: 'Bad Request: Invalid parameters or request syntax.' });
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   try { // this code snippet is using Sequelize to delete a product from a database table based on the provided id in the request parameters. 
-    deletedCategory = await Category.destroy( {where: {id: req.params.id} })
+    deletedCategory = await Category.destroy( { where: {id: req.params.id} })
     res.status(200).json(deletedCategory);
   } catch {
     res.status(500).json({ message: 'Bad Request: Invalid parameters or request syntax.' });
   }
-  
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -11,15 +11,13 @@ res.status(500).json( {message: "Could not find all of the Tags and/or its assoc
 }
 });
 
-router.get('/:id', (req, res) => {
-  try {
-    const singleTag = Tag.findByPk(req.params.id,  {include: [{model: Product}]})
+router.get('/:id', async (req, res) => {
+  try {  // find a single tag by its `id` // be sure to include its associated Product data
+    const singleTag = await Tag.findByPk(req.params.id,  {include: [{model: Product}]})
     res.status(200).json(singleTag)
   } catch {
     res.status(500).json( {message: "Could not find tag"})
   }
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
 });
 
 router.post('/', async (req, res) => {
@@ -32,21 +30,21 @@ router.post('/', async (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-  try {
-
+router.put('/:id', async (req, res) => {
+  try {   // update a tag's name by its `id` value
+    const updatedTag = await Tag.update(req.body, {where: {id: req.params.id}} )
+    res.status(200).json(updatedTag)
   } catch {
-    
+    res.status(500).json({ message: "Failed to update a tag's name by it's id value"})
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-  try {
-
+router.delete('/:id', async (req, res) => {
+  try {   // delete on tag by its `id` value
+    const deletedTag = await Tag.destroy( {where: {id: req.params.id}})
+    res.status(200).json(deletedTag)
   } catch {
-    
+    res.status(500).json( {message: "'Bad Request: Invalid parameters or request syntax.'"})
   }
 });
 
